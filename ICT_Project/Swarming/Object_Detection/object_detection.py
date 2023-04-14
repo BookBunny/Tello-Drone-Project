ec3f4d738126cb04cfb5861fe465cb1b39dbb10a
Jenna <jenna20118@student.hamk.fi>
2023-04-14 05:25:08 +0300

    Swarm object detection

--- ICT_Project/Swarming/Object_Detection/object_detection.py
+++ ICT_Project/Swarming/Object_Detection/object_detection.py
@@ -2,7 +2,7 @@ import math
 import cv2
 import cvzone
 from ultralytics import YOLO
-from djitellopy import tello
+from djitellopy import TelloSwarm
 
 # Classnames for classification
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light",
@@ -18,48 +18,53 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
 # Object to search
 targetObject = "toothbrush"
 
-# Connect to the drone
-drone = tello.Tello()
-drone.connect()
-drone.streamon()
+drones = ['192.168.10.1']
+
+# Create a TelloSwarm object
+swarm = TelloSwarm.fromIps(drones)
+
+# Connect to the drones and start stream
+swarm.connect()
+swarm.streamon()
 
 # Create the model
 model = YOLO("yolov8n.pt")
 
 while True:
-    # Get the image from the drone
-    img = drone.get_frame_read().frame
-    # Resize the image window
-    img = cv2.resize(img, (720, 480))
+    for drone in swarm:
+        # Get the image from the drone
+        img = drone.get_frame_read().frame
+        # Resize the image window
+        img = cv2.resize(img, (720, 480))
 
-    # Get results
-    results = model(img, stream=True)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            # Draw bounding box around each object
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2-x1, y2-y1
-            cvzone.cornerRect(img, (x1, y1, w, h))
+        # Get results
+        results = model(img, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # Draw bounding box around each object
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                w, h = x2-x1, y2-y1
+                cvzone.cornerRect(img, (x1, y1, w, h))
 
-            # Confidence of object classification
-            conf = math.ceil((box.conf[0]*100))/100
-            cvzone.putTextRect(img, f'{conf}', (max(0, x1), max(0, y1)))
+                # Confidence of object classification
+                conf = math.ceil((box.conf[0]*100))/100
+                cvzone.putTextRect(img, f'{conf}', (max(0, x1), max(0, y1)))
 
-            # Class index
-            clsIndex = int(box.cls[0])
-            # Class name
-            cls = classNames[clsIndex]
+                # Class index
+                clsIndex = int(box.cls[0])
+                # Class name
+                cls = classNames[clsIndex]
 
-            # Display class name and confidence
-            cvzone.putTextRect(img, f'{cls} {conf}', (max(0, x1), max(35, y1)))
+                # Display class name and confidence
+                cvzone.putTextRect(img, f'{cls} {conf}', (max(0, x1), max(35, y1)))
 
-            # Check if target object is found
-            if cls == targetObject:
-                print("Object found")
+                # Check if target object is found
+                if cls == targetObject:
+                    print("Object found")
 
 
-    # Show live feed with detected objects
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+        # Show live feed with detected objects
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
