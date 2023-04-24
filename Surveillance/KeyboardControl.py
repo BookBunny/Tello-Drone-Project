a282367f3a8ff680a8ad9a220c398c4574bd2503
BookBunny <75021307+BookBunny@users.noreply.github.com>
2023-04-24 16:00:32 +0300

    Hotfix3

--- Surveillance/KeyboardControl.py
+++ Surveillance/KeyboardControl.py
@@ -1,14 +1,11 @@
 from djitellopy import tello
 import KeyPressModule as kp
-import time
-import cv2
+from time import sleep
 
 kp.init()
 me = tello.Tello()
 me.connect()
 print(me.get_battery())
-global img
-me.streamon()
 
 def getKeyboardInput():
     lr,fb,ud,yv = 0, 0, 0, 0
@@ -26,24 +23,13 @@ def getKeyboardInput():
     if kp.getKey("a"): yv = speed
     elif kp.getKey("d"): yv = -speed
 
-    if kp.getKey("q"): me.land(); time.sleep(3)
+    if kp.getKey("q"): me.land()
     if kp.getKey("e"): me.takeoff()
 
-    if kp.getKey('z'):
-        cv2.imwrite(f'Resources/Images/{time.time()}.jpg', img)
-        time.sleep(0.3)
-
     return [lr,fb,ud,yv]
+
 
 while True:
     vals = getKeyboardInput()
     me.send_rc_control(vals[0],vals[1],vals[2],vals[3])
-    img = me.get_frame_read().frame
-    img = cv2.resize(img, (360, 240))
-    # create a window to display result
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-
-
-
-
+    sleep(0.05)
